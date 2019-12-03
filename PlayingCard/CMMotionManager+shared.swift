b9aee90cc8e471295f0b616766a89afc037be22c
Manson Jones <manson.jones@gmail.com>
2019-12-03 15:51:28 -0800

    Use motion sensors to set gravity in the app.

---
+++ PlayingCard/CMMotionManager+shared.swift
@@ -0,0 +1,13 @@
+//
+//  CMMotionManager+shared.swift
+//  PlayingCard
+//
+//  Created by Manson Jones on 12/3/19.
+//  Copyright © 2019 Manson Jones. All rights reserved.
+//
+
+import CoreMotion
+
+extension CMMotionManager {
+    static var shared = CMMotionManager()
+}
